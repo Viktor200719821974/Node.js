6df3b49034eb39fd093daac8df75db95f2ca23be
Viktor200719821974 <viktorvanyatanya@gmail.com>
2022-03-24 19:32:41 +0100

    lesson 4 successfully

---
+++ lesson4/dist/app.js
@@ -0,0 +1,16 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const users_1 = require("./users");
+const app = (0, express_1.default)();
+console.log(users_1.users);
+app.get('/', (req, res) => {
+    res.end();
+});
+app.listen(5500, () => {
+    console.log('Server has started!!!!!');
+});
+//# sourceMappingURL=app.js.map
