c5d9879da7994aabba255f8dad91616cf4b749b7
Viktor200719821974 <viktorvanyatanya@gmail.com>
2022-04-14 11:50:15 +0200

    lesson 14  100% working

--- lesson14/src/middlewares/fileMiddleware.ts
+++ lesson14/src/middlewares/fileMiddleware.ts
@@ -1,4 +1,4 @@
-import { NextFunction, Response } from 'express';
+import { Response, NextFunction } from 'express';
 import { UploadedFile } from 'express-fileupload';
 import { IRequestExtended } from '../interfaces';
 import { constants } from '../constants';
@@ -11,15 +11,20 @@ class FileMiddleware {
                 next();
                 return;
             }
+
             const { name, size, mimetype } = req.files.avatar as UploadedFile;
+
             if (size > constants.PHOTO_MAX_SIZE) {
                 next(new ErrorHandler(`File ${name} is too big`));
                 return;
             }
+
             if (!constants.PHOTOS_MIMETYPES.includes(mimetype)) {
                 next(new ErrorHandler('Wrong file format'));
                 return;
             }
+
+            next();
         } catch (e) {
             next(e);
         }
--- lesson14/src/services/s3.service.ts
+++ lesson14/src/services/s3.service.ts
@@ -2,6 +2,7 @@ import S3, { ManagedUpload } from 'aws-sdk/clients/s3';
 import { UploadedFile } from 'express-fileupload';
 import path from 'path';
 import { v4 as uuidv4 } from 'uuid';
+
 import { config } from '../config/config';
 
 class S3Service {
@@ -18,19 +19,21 @@ class S3Service {
     uploadFile(file: UploadedFile, itemType: string, itemId: number)
         : Promise<ManagedUpload.SendData> {
         const uploadFilePath = this.fileNameBuilder(file.name, itemType, itemId);
+
         return this.Bucket.upload({
             Bucket: config.S3_NAME as string,
             Body: file.data,
             Key: uploadFilePath,
             ContentType: file.mimetype,
-            // ACL: 'public-read',
+            ACL: 'public-read',
         })
-            .promise();
+            .promise(); // !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
     }
 
     private fileNameBuilder(fileName: string, itemType: string, itemId: number): string {
-        const fileExtention = path.extname(fileName);
-        return `${itemType}/${itemId}/${uuidv4()}${fileExtention}`;
+        const fileExtension = path.extname(fileName); // .png
+
+        return `${itemType}/${itemId}/${uuidv4()}${fileExtension}`;
     }
 }
 
